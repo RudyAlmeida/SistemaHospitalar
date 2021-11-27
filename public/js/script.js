4cd4b13d0c90077de6dad0aa557410a964e92e7f
Rodolpho Rodrigues Almeida <rodolpho_almeida@hotmail.com>
2021-11-27 19:43:44 -0300

    bug fix

--- public/js/script.js
+++ public/js/script.js
@@ -38,7 +38,7 @@ function signOut() {
     auth2.signOut().then(function() {
         document.getElementById('dropText').innerText = "Cadastre-se / Login"
         let logImg = document.getElementById('loginImg')
-        logImg.src = "imagens/person-circle.svg"
+        logImg.src = "img/person-circle.svg"
         localStorage.clear();
         console.log('User signed out.');
         console.log(localStorage)
